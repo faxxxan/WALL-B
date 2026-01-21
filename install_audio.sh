8dc2e908805b7af806dfa115098cc64e5dcc5496
danic85 <dan.nicholson@outlook.com>
2026-01-21 21:29:50 +0000

    WIP

---
+++ install_audio.sh
@@ -0,0 +1,7 @@
+
+sudo myenv/bin/python3 -m pip install --upgrade click
+
+sudo myenv/bin/python3 -m pip install --upgrade setuptools
+sudo myenv/bin/python3 -m pip install --upgrade adafruit-python-shell
+
+sudo myenv/bin/python3 installers/i2samp.py
